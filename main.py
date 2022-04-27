f356bdd3ea38caeab0a46b917a2a6cef841d44e3
sergey.dmitriev <info1981@yandex.ru>
2022-04-27 22:36:03 +0300

    version 1.0.0

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Программа скачивает файлы по адресам указанным в urls.txt и помещает их в директорию download
+В файле urls.txt, на последней строке, должен стоять перевод строки
+"""
+
+import wget
+
+
+f = open('urls.txt', 'r', encoding='utf8')
+lst = f.readlines()
+f.close()
+for i in range(0, len(lst)):
+    url = lst[i][:-1]
+    print(url)
+    index = lst[i].rfind('/')
+    print(lst[i][index + 1:-1])
+    wget.download(url, f'./download/{lst[i][index + 1:-1]}')
+
